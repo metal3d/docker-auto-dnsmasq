59267910f784f84840d1f2beefe839b75c60230a
Patrice Ferlet <metal3d@gmail.com>
2022-04-06 11:06:50 +0200

    Code clean, manage execption

--- configure-docker.py
+++ configure-docker.py
@@ -1,19 +1,19 @@
 import json
-import subprocess
-import re
 import os
+import re
+import subprocess
 import sys
 
-INTERFACE = 'docker0'
-DAEMONFILE = '/etc/docker/daemon.json'
-NM_DOCKER_CONFIG = '/etc/NetworkManager/dnsmasq.d/docker-bridge.conf'
+INTERFACE = "docker0"
+DAEMONFILE = "/etc/docker/daemon.json"
+NM_DOCKER_CONFIG = "/etc/NetworkManager/dnsmasq.d/docker-bridge.conf"
 
-CIDR = os.environ.get('CIDR', '172.0.0.0/8')
+CIDR = os.environ.get("CIDR", "172.0.0.0/8")
 
 # find docker0 ip
-ipa = subprocess.run(['ip', 'a', 'show', INTERFACE], stdout=subprocess.PIPE)
+ipa = subprocess.run(["ip", "a", "show", INTERFACE], stdout=subprocess.PIPE)
 ipa = ipa.stdout.decode()
-ipa = re.findall(r'inet ([0-9\.]+)', ipa)
+ipa = re.findall(r"inet ([0-9\.]+)", ipa)
 
 assert len(ipa) > 0
 
@@ -23,26 +23,29 @@ ipa = ipa[0]
 daemon = {}
 if os.path.exists(DAEMONFILE):
     try:
-        with open(DAEMONFILE, 'r') as d:
+        with open(DAEMONFILE, "r") as d:
             daemon = json.load(d)
     except Exception:
         print(
-            '%s file seems to be invalid, '
-            'please take a look and retry later' % DAEMONFILE
+            "%s file seems to be invalid, "
+            "please take a look and retry later" % DAEMONFILE
         )
-        os.exit(1)
+        sys.exit(1)
 else:
-    print('docker daemon configuration not found, we will create it')
+    print("docker daemon configuration not found, we will create it")
 
 
 # find dns configuration
-dns = daemon.get('dns', [])
+if "dns" in daemon:
+    dns = daemon["dns"]
+else:
+    dns = []
 
 if "remove" in sys.argv:
     # if we want to remove dns,
     # so we remove the entry in dns list
     if ipa in dns:
-        del(dns[dns.index(ipa)])
+        del dns[dns.index(ipa)]
 
     # remove the NetworkManager file
     # that activates dnsmasq on docker0 interface
@@ -58,17 +61,20 @@ else:
 
     # write configuration to NetworkManager
     # to listen on docker0
-    with open(NM_DOCKER_CONFIG, 'w') as nm:
-        conf = 'listen-address=%s' % ipa
+    with open(NM_DOCKER_CONFIG, "w") as nm:
+        conf = "listen-address=%s" % ipa
         nm.write(conf)
 
 # replace dns list
 if len(dns) > 0:
-    daemon['dns'] = dns
+    daemon["dns"] = dns
 else:
     # no dns, remove the option
-    del(daemon['dns'])
+    try:
+        del daemon["dns"]
+    except:
+        pass
 
 # write configuration
-with open(DAEMONFILE, 'w') as d:
-    json.dump(daemon, d, indent='    ')
+with open(DAEMONFILE, "w") as d:
+    json.dump(daemon, d, indent="    ")
